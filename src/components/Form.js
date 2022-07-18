a2f322b37abbdc7b71578bf2e592677115139922
ndalilavin <vinson.steve@student.moringaschool.com>
2022-07-19 00:23:13 +0300

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form(props) {
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" 
+      onChange={props.handleFirstNameChange}
+      value={props.lastName}
+      />
+
+      <input 
+      type="text" 
+      onChange={props.handleLastNameChange}
+      value={props.lastName}
+      />
+
       <button type="submit">Submit</button>
     </form>
   );
